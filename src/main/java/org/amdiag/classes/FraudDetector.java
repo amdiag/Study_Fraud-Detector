ae1f43eeaee56c08341202d5fdfa018227ada967
amdiag <amdiag@ya.ru>
2024-09-19 09:57:26 +0300

    Improving FraudDetector class

---
+++ src/main/java/org/amdiag/classes/FraudDetector.java
@@ -0,0 +1,20 @@
+package org.amdiag.classes;
+
+import org.amdiag.classes.fraudRule.*;
+import org.amdiag.intarfaces.FraudRule;
+
+public class FraudDetector {
+    private FraudRule rule1 = new FraudRule1();
+    private FraudRule rule2 = new FraudRule2();
+    private FraudRule rule3 = new FraudRule3();
+    private FraudRule rule4 = new FraudRule4();
+    private FraudRule rule5 = new FraudRule5();
+
+    boolean isFraud(Transaction transaction) {
+        return rule1.isFraud(transaction)
+                || rule2.isFraud(transaction)
+                || rule3.isFraud(transaction)
+                || rule4.isFraud(transaction)
+                || rule5.isFraud(transaction);
+    }
+}
